39814b0123363c8392caa5ec276253fbf4a72f0e
Philipp Nueesch <phil@rhinerock.com>
2015-05-09 22:51:57 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,218 @@
+#! /usr/bin/env node
+
+/**
+ * Saja - CLI for Sassyjade Boilerplate
+ *
+ * @license MIT
+ * @author Philipp Nueesch <phil@rhinerock.com> (http://rhinerock.com)
+ */
+
+// to run commands in terminal
+var exec = require("child_process").exec;
+
+// get the user options
+var userArgs = process.argv.splice(2);
+
+// pass the return from the check on userArgs
+var args = checkUserArgs(userArgs);
+
+// donwload the files and install everything
+downloadZip();
+
+
+// Utilities
+
+/**
+ * checks if user args are valid
+ * @params arr user provided arguments
+ * @return obj with valid user arguments
+ */
+function checkUserArgs(userArgs) {
+  
+  var blankFlag = false;
+  var projName = "";
+  var errStack = [];
+  var patt = /^[a-zA-Z0-9_-]+$/
+
+  // if more than 2 arguments add err to stack
+  if(userArgs.length > 2) {
+    errStack.push("Too many arguments. Make sure to pass a name for your project and the --blank flag if needed only.");
+  }
+
+  // if less than 1 argument add err to stack
+  if(userArgs.length < 1) {
+    errStack.push("Please give your project a name.");
+  }
+
+  // if 2 arguments check that 1 is either --blank or -b
+  if(userArgs.length === 2) {
+    if(userArgs[0] === "--blank" || userArgs[0] === "-b") {
+      // and the other is a valid string
+      if(patt.test(userArgs[1])) {
+        // then set blankFlag to true and projName to the string provided
+        blankFlag = true;
+        projName = userArgs[1];
+      } else {     
+        errStack.push("Make sure you pass a string to name your project. Characters, numbers, underscore and dash are supported only.");
+      } 
+    } else if(userArgs[1] === "--blank" || userArgs[1] === "-b") {
+      if(patt.test(userArgs[0])) {
+        blankFlag = true;
+        projName = userArgs[0];
+      } else {
+        errStack.push("Make sure you pass a string to name your project. Characters, numbers, underscore and dash are supported only.");
+      }
+    } else {
+      errStack.push("You can pass a string to name your project and the --blank flag only. For the name characters, numbers, underscore and dash are supported.");
+    }
+  }
+
+  // if 1 argument is passed make sure it is a valid string
+  if(userArgs.length === 1) {
+    if(patt.test(userArgs[0])) {
+      blankFlag = false;
+      projName = userArgs[0];
+    } else {
+      errStack.push("Make sure you pass a string to name your project. Characters, numbers, underscore and dash are supported only.");
+    }
+  }
+
+  // if errStack.length is < 1
+  // then return projName and blankFlag as properties in args obj
+  // else loop through errStack and console.log all err
+  if(errStack.length === 0) {
+    var args = {
+      name: projName,
+      blank: blankFlag
+    }
+    return args;
+  } else {
+    for(var i = 0; i < errStack.length; i++) {
+      console.log("--ERR: ", errStack[i],"--");
+    }
+    return false;
+  }
+
+} // func checkUserArgs()
+
+
+/**
+ * Downloads the zip file and kicks-off the unZip()
+ * @return bool true if no errors
+ * @callback unZip()
+ */
+function downloadZip() {
+  var url = "https://github.com/philister16/sassyjade/archive/master.zip";
+  var target = __dirname + "/master.zip";
+  var cmd = "curl -L -o " + target + " " + url;
+  console.log("Loading...");
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) {
+      throw err;
+      return false;
+    } else {
+      unZip();
+      return true;
+    }
+  });
+}
+
+/**
+ * Unzips the download
+ * @return bool true if no err
+ * @callback rmZip()
+ */
+function unZip() {
+  var target = __dirname + "/master.zip";
+  var cmd = "unzip " + target;
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) {
+      throw err;
+      return false;
+    } else {
+      rmZip();
+      return true;
+    }
+  });
+}
+
+/**
+ * Removes the zip file after unzipped
+ * @return bool true if no err
+ * @callback rename()
+ */
+function rmZip() {
+  var target = __dirname + "/master.zip";
+  var cmd = "rm " + target;
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) {
+      throw err;
+      return false;
+    } else {
+      rename(args);
+      return true;
+    }
+  });
+}
+
+/**
+ * Renames the directory
+ * @param obj arguments provided by the user
+ * @return bool true if no err
+ * @callback npmInstalls() || makeBlank()
+ */
+function rename(args) {
+  var target = __dirname + "/sassyjade-master";
+  var cmd = "mv " + target + " " + __dirname + "/" + args.name;
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) {
+      throw err;
+      return false;
+    } else if(args.blank) {
+      makeBlank(args);
+    } else {
+      console.log("... hang on, installing the dependencies now. This can take a few minutes.");
+      npmInstalls(args);
+      return true;
+    }
+  });
+}
+
+/**
+ * Removes all folders
+ * @param obj user provided arguments
+ * @return bool true if no err
+ */
+function makeBlank(args) {
+  var target = __dirname + "/" + args.name;
+  var cmd = "cd " + target + " && rm -r src";
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) {
+      throw err;
+      return false;
+    } else {
+      console.log("... and done! Your blank project is ready.")
+      return true;
+    }
+  });
+}
+
+/**
+ * Install the dev-dependencies
+ * @param obj arguments provided by user
+ * @ return bool true if no err
+ */
+function npmInstalls(args) {
+  var target = __dirname + "/" + args.name;
+  var cmd = "cd " + target + " && npm install";
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) {
+      throw err;
+      return false;
+    } else {
+      console.log("... and done!");
+      return true;
+    }
+  });
+}
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Saja
+A super small CLI for Sassyjade
+## Usage
+```
+$ saja <yourProject>
+```
+This command will fetch the latest version of Sassyjade boilerplate and install all its dependencies in a new folder called "your Project", relative to the folder from where you ran the command.
+```
+$ saja --blank <yourProject>
+```
+If you don't want the default folder and file structre of Sassyjade add the `--blank` flag. You can also just use `-b` instead.
+## License
+MIT &copy; [philister16](mailto:phil@rhinerock.com)
